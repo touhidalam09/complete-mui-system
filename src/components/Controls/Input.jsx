e8b348f86c965dcced52d902bcffe6670a1ad623
touhidalam09 <touhidalam649@gmail.com>
2022-01-03 04:17:37 +0600

    Validation added validate or validationOnChange

--- src/components/Controls/Input.jsx
+++ src/components/Controls/Input.jsx
@@ -1,18 +1,19 @@
-import { TextField } from '@mui/material';
-import React from 'react';
+import { TextField } from "@mui/material";
+import React from "react";
 
 function Input(props) {
-    const { label, name, value, onChange } = props;
-    return (
-        <TextField
-            id='outlined-basic'
-            variant='outlined'
-            label={label}
-            name={name}
-            value={value}
-            onChange={onChange}
-        />
-    );
+  const { label, name, value, onChange, error = null } = props;
+  return (
+    <TextField
+      id="outlined-basic"
+      variant="outlined"
+      label={label}
+      name={name}
+      value={value}
+      onChange={onChange}
+      {...(error && { error: true, helperText: error })}
+    />
+  );
 }
 
 export default Input;
--- src/pages/Employee/EmployeeForm.jsx
+++ src/pages/Employee/EmployeeForm.jsx
@@ -1,108 +1,141 @@
-import { Grid } from '@mui/material';
-import React from 'react';
-import Controls from '../../components/Controls/Controls';
-import { useForm, Form } from '../../components/useForm';
-import * as employeeService from '../../services/employeeService'
+import { Grid } from "@mui/material";
+import React from "react";
+import Controls from "../../components/Controls/Controls";
+import { useForm, Form } from "../../components/useForm";
+import * as employeeService from "../../services/employeeService";
 
 const genderItems = [
-    { id: 'male', title: 'Male' },
-    { id: 'female', title: 'Female' },
-    { id: 'other', title: 'Other' },
+  { id: "male", title: "Male" },
+  { id: "female", title: "Female" },
+  { id: "other", title: "Other" },
 ];
 
-const intializeFValue = {
-    id: '',
-    fullName: '',
-    email: '',
-    mobile: '',
-    city: '',
-    gender: 'male',
-    departmentID: '',
-    hireDate: new Date(),
-    isParmanent: false
-}
+const initializeFValue = {
+  id: "",
+  fullName: "",
+  email: "",
+  mobile: "",
+  city: "",
+  gender: "male",
+  departmentID: "",
+  hireDate: new Date(),
+  isPermanent: false,
+};
 
 function EmployeeForm() {
+  //Form validation
 
+  const validate = (fieldValues = values) => {
+    let temp = {};
 
-    const {
-        values,
-        setValues,
-        handleInputChange
-    } = useForm(intializeFValue)
+    if ("fullName" in fieldValues)
+      temp.fullName =
+        fieldValues.fullName.length > 2 ? "" : "This field is required.";
+    if ("email" in fieldValues)
+      temp.email = /$^|.+@.+..+/.test(fieldValues.email)
+        ? ""
+        : "Email is not valid.";
+    if ("mobile" in fieldValues)
+      temp.mobile =
+        fieldValues.mobile.length > 10 ? "" : "Minimum 11 number required";
+    if ("departmentID" in fieldValues)
+      temp.departmentID =
+        fieldValues.departmentID.length !== 0 ? "" : "This field is required.";
+    setErrors({
+      ...temp,
+    });
 
-    return (
-        <>
-            <Form>
-                <Grid container>
-                    <Grid item xs={6}>
-                        <Controls.Input
-                            label='Full name'
-                            name="fullName"
-                            value={values.fullName}
-                            onChange={handleInputChange}
-                        />
-                        <Controls.Input
-                            label='Email'
-                            name="email"
-                            value={values.email}
-                            onChange={handleInputChange}
-                        />
-                        <Controls.Input
-                            label='Mobile'
-                            name="mobile"
-                            value={values.mobile}
-                            onChange={handleInputChange}
-                        />
-                        <Controls.Input
-                            label='City'
-                            name="city"
-                            value={values.city}
-                            onChange={handleInputChange}
-                        />
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Controls.RadioGroup
-                            label='Gender'
-                            name="gender"
-                            value={values.gender}
-                            onChange={handleInputChange}
-                            items={genderItems}
-                        />
-                        <Controls.Select
-                            label='Department'
-                            name='departmentID'
-                            value={values.departmentID}
-                            onChange={handleInputChange}
-                            options={employeeService.getDepartmentCollection()}
-                        />
-                        {/* <Controls.DatePickers
+    if (fieldValues === values)
+      return Object.values(temp).every((x) => x === "");
+  };
+
+  //Reuseable Form calling
+  const { values, setValues, errors, setErrors, handleInputChange, resetForm } =
+    useForm(initializeFValue, true, validate); //if you onchange validation want pass true otherwise pass false
+
+  //After submit form handler
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validate()) {
+      console.log(validate());
+      window.alert("Validated");
+    }
+  };
+
+  return (
+    <>
+      <Form onSubmit={handleSubmit}>
+        <Grid container>
+          <Grid item xs={6}>
+            <Controls.Input
+              label="Full name"
+              name="fullName"
+              value={values.fullName}
+              onChange={handleInputChange}
+              error={errors.fullName}
+            />
+            <Controls.Input
+              label="Email"
+              name="email"
+              value={values.email}
+              onChange={handleInputChange}
+              error={errors.email}
+            />
+            <Controls.Input
+              label="Mobile"
+              name="mobile"
+              value={values.mobile}
+              onChange={handleInputChange}
+              error={errors.mobile}
+            />
+            <Controls.Input
+              label="City"
+              name="city"
+              value={values.city}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <Controls.RadioGroup
+              label="Gender"
+              name="gender"
+              value={values.gender}
+              onChange={handleInputChange}
+              items={genderItems}
+            />
+            <Controls.Select
+              label="Department"
+              name="departmentID"
+              value={values.departmentID}
+              onChange={handleInputChange}
+              options={employeeService.getDepartmentCollection()}
+              error={errors.departmentID}
+            />
+            {/* <Controls.DatePickers
                             label="Hire Date"
                             name="hireDate"
                             value={values.hireDate}
                             onChange={handleInputChange}
                         /> */}
-                        <Controls.Checkbox
-                            label="Parmanent Employee"
-                            name="isParmanent"
-                            value={values.isParmanent}
-                            onChange={handleInputChange}
-                        />
-                        <div>
-                            <Controls.Button
-                                type="submit"
-                                text="Submit"
-                            />
-                            <Controls.Button
-                                text="Reset"
-                                color="primary"
-                            />
-                        </div>
-                    </Grid>
-                </Grid>
-            </Form>
-        </>
-    );
+            <Controls.Checkbox
+              label="Parmanent Employee"
+              name="isParmanent"
+              value={values.isParmanent}
+              onChange={handleInputChange}
+            />
+            <div>
+              <Controls.Button type="submit" text="Submit" />
+              <Controls.Button
+                text="Reset"
+                color="primary"
+                onClick={resetForm}
+              />
+            </div>
+          </Grid>
+        </Grid>
+      </Form>
+    </>
+  );
 }
 
 export default EmployeeForm;
