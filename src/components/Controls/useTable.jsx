00cecbee717097b8e8ed67ebace6e647dd2e2a7d
touhidalam09 <touhidalam649@gmail.com>
2022-01-10 03:06:39 +0600

    table ordering asc and desc

--- src/components/Controls/useTable.jsx
+++ src/components/Controls/useTable.jsx
@@ -90,8 +90,39 @@ function useTable(records, headCells) {
     />
   );
 
+  function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  }
+
+  function getComparator(order, orderBy) {
+    return order === "desc"
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy);
+  }
+
+  function stableSort(array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) {
+        return order;
+      }
+      return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+  }
+
   const recordsAfterPagingAndSorting = () => {
-    return records.slice(page * rowsPerPage, (page + 1) * rowsPerPage);
+    return stableSort(records, getComparator(order, orderBy)).slice(
+      page * rowsPerPage,
+      (page + 1) * rowsPerPage
+    );
   };
 
   return { TblContainer, TblHead, TblPagination, recordsAfterPagingAndSorting };
