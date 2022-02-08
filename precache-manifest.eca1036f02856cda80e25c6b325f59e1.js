534735580bc660fbf1a2ea6dad4b603658a80c46
touhidalam09 <touhidalam649@gmail.com>
2022-02-08 18:27:38 +0600

    Updates

---
+++ precache-manifest.eca1036f02856cda80e25c6b325f59e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80340358acb60322ae850b5a88178f7e",
+    "url": "/touhidalam09/complete_mui_system/index.html"
+  },
+  {
+    "revision": "5b891d9925a13b3c2064",
+    "url": "/touhidalam09/complete_mui_system/static/css/main.0dedab61.chunk.css"
+  },
+  {
+    "revision": "382d5f98017bcac20ccc",
+    "url": "/touhidalam09/complete_mui_system/static/js/2.32204227.chunk.js"
+  },
+  {
+    "revision": "fe51ce782595955903811365759820e4",
+    "url": "/touhidalam09/complete_mui_system/static/js/2.32204227.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f0148a4ae8686a0845c",
+    "url": "/touhidalam09/complete_mui_system/static/js/3.afa10aa8.chunk.js"
+  },
+  {
+    "revision": "5b891d9925a13b3c2064",
+    "url": "/touhidalam09/complete_mui_system/static/js/main.78cdf5d2.chunk.js"
+  },
+  {
+    "revision": "b933d5c8f4fc1113fd48",
+    "url": "/touhidalam09/complete_mui_system/static/js/runtime-main.d9d64c2e.js"
+  }
+]);
